c022d61298167a751c44429428616b85117043ac
Jeymerd Canales <jeymerdvanales98@gmail.com>
2020-03-10 14:28:44 -0600

    Prueba de inicializacion

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        int numero[] = new int[7];
+        for (int i = 0; i < numero.length ; i++) {
+            numero[i] = (int) (Math.random() * 6 + 1);
+            System.out.println(" " + numero[i]);
+        }
+        for (int i = 0; i < numero.length; i++) {
+        }
     }
 }
